8b98a40a7daad278e82b0aa8604322f03ac0a5bb
Akasharyan47 <akasharyan.aryan@gamil.com>
2024-02-10 21:54:06 +0530

    commit

--- src/components/ADMISSION/Admission.js
+++ src/components/ADMISSION/Admission.js
@@ -15,7 +15,9 @@ import { FooterBottom } from '../FOOTER/Footer.jsx';
           </div>
           <div className="admprocess2">
             <span className="adma text-decoration-none">
-              <svg className='circlei ' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">  <path d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512zM216 336h24V272H216c-13.3 0-24-10.7-24-24s10.7-24 24-24h48c13.3 0 24 10.7 24 24v88h8c13.3 0 24 10.7 24 24s-10.7 24-24 24H216c-13.3 0-24-10.7-24-24s10.7-24 24-24zm40-144c-17.7 0-32-14.3-32-32s14.3-32 32-32s32 14.3 32 32s-14.3 32-32 32z" /></svg>
+
+              <svg className='circlei ' viewBox="0 0 512 512">  <path d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512zM216 336h24V272H216c-13.3 0-24-10.7-24-24s10.7-24 24-24h48c13.3 0 24 10.7 24 24v88h8c13.3 0 24 10.7 24 24s-10.7 24-24 24H216c-13.3 0-24-10.7-24-24s10.7-24 24-24zm40-144c-17.7 0-32-14.3-32-32s14.3-32 32-32s32 14.3 32 32s-14.3 32-32 32z" /></svg>
+
               Currently, we are accepting offline admission forms. please visit our school/call us to know
               more about the admission process.
             </span>
@@ -122,8 +124,8 @@ letter-spacing: 0.5px;
 } 
 .admprocess .admprocess2 .circlei {
  height: fit-content;
- max-width: 2.5rem;
- margin-right: 23px;
+ max-width: 25px;
+ margin-right: 10px;
  margin-left: 5px; 
  fill: ${({ theme }) => theme.colors.backgroundYellow};  
 } 
